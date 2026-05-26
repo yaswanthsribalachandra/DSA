b249c3e6e6c76771e94921204da88f82a01f6cba
Dasari Yaswanth Sri Balachandra <22331a4415@mvgrce.edu.in>
2026-05-26 16:40:50 +0530

    Added searching and sorting algorithms

---
+++ searching/binarysearch.py
@@ -0,0 +1,15 @@
+def binarysearch(arr,target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+a = binarysearch([1, 2, 3, 4, 5], 3)
+print(a)
---
+++ searching/linearsearch.py
@@ -0,0 +1,7 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+a = linear_search([1, 2, 3, 4, 5], 3)
+print(a)
